0788f87c7d591c2cb6088b3cb4dea1a243759212
deveshXm <deveshm.cs.20@nitj.ac.in>
2022-06-12 18:28:35 +0530

    About component using typwriter-effect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,27 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import About from "../components/About";
+import Navbar from "../components/Navbar";
 
 const Home: NextPage = () => {
+  
   return (
-    <div className="">
+    <div className="font-body">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Porfolio | Home</title>
       </Head>
-      <div className='bg-[#202020] h-screen'>
-        
-      </div>
-      <div className='bg-white h-screen'>
-
-      </div>
+      <Navbar />
+      <main className="font-body" id= "myCanvas">
+        <div className="bg-[#202020] h-screen content-center flex flex-col items-center justify-center">
+          <h1 className="absolute text-[#5D3FD3] font-semibold  mb-[20%] text-4xl mr-[60%] ">
+            &lt;Body&gt;
+          </h1>
+          <About />
+        </div>
+        <div className="bg-[#5D3FD3] h-screen"></div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
