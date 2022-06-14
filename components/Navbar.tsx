35a56ae058a0224e149aea7bd6a7ed9a7142a542
deveshXm <deveshm.cs.20@nitj.ac.in>
2022-06-14 14:55:39 +0530

    responsive navbar update-1

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,27 @@
 import React from "react";
+import Link from "next/link";
 
 function Navbar() {
-  return <div className="fixed top-0 z-[10] w-full text-white left items-center pr-24 py-6 ">
-    <ul className="flex space-x-12 ">
-      <li className=" flex ml-auto"><p className=" text-[#5D3FD3]">log</p>.About()</li>
-      <li className=" flex ml-auto"><p className=" text-[#5D3FD3]">log</p>.Projects()</li>
-      <li className=" flex ml-auto"><p className=" text-[#5D3FD3]">log</p>.Experience()</li>
-      <li className=" flex ml-auto"><p className=" text-[#5D3FD3]">log</p>.Contact()</li>
-    </ul>
-  </div>;
+  return (
+    <div>
+      <div className="hidden md:block fixed top-0 z-[10] w-full text-white left items-center pr-24 py-10 ">
+        <ul className="flex space-x-12 ">
+          <li className=" flex ml-auto">
+            <p className=" text-[#5D3FD3]">log</p>.About()
+          </li>
+          <li className=" flex ml-auto">
+            <p className=" text-[#5D3FD3]">log</p>.Projects()
+          </li>
+          <li className=" flex ml-auto">
+            <p className=" text-[#5D3FD3]">log</p>.Experience()
+          </li>
+          <li className=" flex ml-auto">
+            <p className=" text-[#5D3FD3]">log</p>.Contact()
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
