1acaa9e302a44c3f676d116992f080cb8c3337a5
deveshXm <deveshm.cs.20@nitj.ac.in>
2022-06-17 21:12:34 +0530

    changes

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -5,14 +5,14 @@ function Experience() {
   const experience = [
     {
       id: "p1",
-      heading: "Microsoft Engage",
+      heading: "Microsoft Engage 2022",
       time: "May 2022",
       info: "In one month period of microsoft engage i built a movie recommendation algorithm which recommended movies to user based on last watch. ",
       language: "Internship",
     },
   ];
   return (
-    <div className="relative bg-[#5D3FD3] bg-opacity-40 py-32">
+    <div className="relative bg-white py-32">
       <div className="flex justify-end px-2 text-[#202020] text-[16.5vw] font-sans font-bold ">
         <p>EXPERIENCE</p>
       </div>
--- components/ExperienceCard.js
+++ components/ExperienceCard.js
@@ -5,10 +5,10 @@ function Card(props) {
   return (
     <div className="ml-auto mr-auto py-7 px-5 xl:px-10 w-[90%] md:w-[70%] border border-gray-400 bg-[#202020] ">
       <div className="flex items-center justify-between">
-        <h1 className="text-2xl sm:text-5xl font-extrabold">{props.heading}</h1>
+        <h1 className="text-2xl sm:text-5xl font-extrabold text-[#5D3FD3]">{props.heading}</h1>
         <p className="text-xs xl:text-xl text-gray-400">{props.time}</p>
       </div>
-      <div className="flex py-5 text-gray-400">{props.language}</div>
+      <div className="flex py-5 text-gray-400 text-2xl">{props.language}</div>
       <div>
         <p className="md:text-2xl">{props.info}</p>
       </div>
